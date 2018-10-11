600d0cd836946e08a5f603e428a161b1755c121e
Stefan Maierhofer <sm@stefanmaierhofer.com>
2018-10-11 07:21:35 +0200

    humgui [-p <port>] [-gl|-ogl]

--- src/humgui/Program.fs
+++ src/humgui/Program.fs
@@ -1,34 +1,52 @@
 open gumgui
 
 open Aardium
+open Aardvark.Application.Slim
+open Aardvark.Base
+open Aardvark.Rendering.Vulkan
 open Aardvark.Service
 open Aardvark.UI
 open Suave
 open Suave.WebPart
-open Aardvark.Rendering.Vulkan
-open Aardvark.Base
 open System
-
-
-
 
 [<EntryPoint>]
 let main args =
+
+    let mutable port = 4321
+    let mutable useVulkan = true
+    let mutable i = 0
+    while i < args.Length do
+        match args.[i] with
+        | "-port" | "-p" -> i <- i + 1; port <- Int32.Parse args.[i]
+        | "-gl" | "-ogl" -> useVulkan <- false
+        | _ -> Log.warn "unknown argument: %s" args.[i]
+
+        i <- i + 1
+
     Ag.initialize()
     Aardvark.Init()
     Aardium.init()
-
-    let app = new HeadlessVulkanApplication(true)
-
-    WebPart.startServer 4321 [
-        MutableApp.toWebPart' app.Runtime false (App.start App.app)
-    ]
     
+    if useVulkan then
+
+        let app = new HeadlessVulkanApplication(true)
+        WebPart.startServer port [
+            MutableApp.toWebPart' app.Runtime false (App.start App.app)
+        ]
+
+    else
+
+        let app = new OpenGlApplication(true)
+        WebPart.startServer port [
+            MutableApp.toWebPart' app.Runtime false (App.start App.app)
+        ]
+
     Aardium.run {
-        title "Aardvark rocks \\o/"
+        title "hum - a viewer for humongous point clouds"
         width 1024
         height 768
-        url "http://localhost:4321/"
+        url (sprintf "http://localhost:%i/" port)
     }
 
     0
