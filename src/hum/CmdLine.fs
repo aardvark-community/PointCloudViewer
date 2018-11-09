3df3c784eb86593616f5cb8f4dffd01cc2ff2cc8
Stefan Maierhofer <sm@stefanmaierhofer.com>
2018-11-09 08:48:38 +0100

    update packages

--- src/hum/CmdLine.fs
+++ src/hum/CmdLine.fs
@@ -13,27 +13,30 @@
 *)
 namespace hum
 
+open Aardvark.Application
+open Aardvark.Application.Slim
+open Aardvark.Base
+open Aardvark.Base.Incremental
+open Aardvark.Base.Rendering
+open Aardvark.Data.Points
+open Aardvark.Data.Points.Import
+open Aardvark.Geometry
+open Aardvark.Geometry.Points
+open Aardvark.Rendering.Vulkan
+open Aardvark.SceneGraph
+open Newtonsoft.Json.Linq
 open System
 open System.Collections.Generic
 open System.IO
 open System.Linq
 open System.Net
 open System.Threading
-open Aardvark.Base
-open Aardvark.Base.Incremental
-open Aardvark.Base.Rendering
-open Aardvark.Rendering.Vulkan
-open Aardvark.SceneGraph
-open Aardvark.Application
-open Aardvark.Application.Slim
-open Aardvark.Geometry.Points
-open Aardvark.Data.Points
-open Aardvark.Data.Points.Import
-open Newtonsoft.Json.Linq
 open Uncodium.SimpleStore
 
 [<AutoOpen>]
 module CmdLine = 
+    open Uncodium
+    open System.Diagnostics
 
     let parseBounds filename =
         let json = JArray.Parse(File.readAllText filename) :> seq<JToken>
@@ -208,6 +211,49 @@ module CmdLine =
         Console.WriteLine("point count   {0:N0}", info.PointCount)
         Console.WriteLine("bounds        {0}", info.Bounds)
 
+    let private foo (chunks : seq<Chunk>) =
+        chunks
+        |> Seq.map (fun chunk ->
+        
+            let k = 27
+            let ps = chunk.Positions.ToArray()
+            let cs = chunk.Colors.ToArray()
+
+            let sw = Stopwatch()
+            sw.Restart()
+            let kd = ps.CreateRkdTree(Metric.Euclidean, 0.0)
+            sw.Stop()
+            printfn "kd-tree creation: %A" sw.Elapsed
+
+            sw.Restart()
+            let median = ps.Sum() / float ps.Length
+            let cs = ps
+                     |> Array.map2 (fun (c : C4b) p ->
+                     
+                        if p.Z < median.Z then
+                            c
+                        else
+                            let closest = kd.GetClosest(p, 0.30, k)
+                            
+                            if closest.Count < 8 then
+                                c
+                            else
+                                let mutable q = M33d.Zero
+                                let mutable w = V3d.Zero
+                                let mutable cvm = ps.ComputeCovarianceMatrix(closest, p)
+                                let mutable o : int[] = null
+                                if Eigensystems.Dsyevh3asc(&cvm, &q, &w, &o) then
+                                    let flatness = 1.0 - w.[o.[0]] / w.[o.[1]]
+                                    let verticality = V3d.Dot(q.Column(o.[0]).Normalized.Abs, V3d.ZAxis)
+                                    if flatness > 0.99 && verticality > 0.7 then C4b(255uy, c.G / 2uy, c.B / 2uy) else c
+                                else
+                                    c
+                        ) cs
+            sw.Stop()
+            printfn "classification: %A" sw.Elapsed
+            chunk.WithColors(cs)
+            )
+
     let private mapReduce cfg (chunks : seq<Chunk>) = PointCloud.Chunks(chunks, cfg)
     let private batchImport dirname (cfg : ImportConfig) (args : Args) =
         getKnownFileExtensions ()
@@ -253,7 +299,14 @@ module CmdLine =
             match isBatchImport, args.asciiFormat with
             
             // single file, known format
-            | false, None   -> PointCloud.Import(filename, cfg)
+            | false, None   -> let sw = Stopwatch()
+                               sw.Restart()
+                               let chunks = Import.Ply.Chunks(filename, cfg) |> foo
+                               sw.Stop()
+                               printfn "parsing: %A" sw.Elapsed
+                               PointCloud.Chunks(chunks, cfg)
+                               
+                               // PointCloud.Import(filename, cfg)
                  
             // single file, custom ascii
             | false, Some f -> let chunks = Import.Ascii.Chunks(filename, f, cfg)
